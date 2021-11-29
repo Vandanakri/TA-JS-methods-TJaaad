813907ca79c5dd8be494bb86596f7dd256d9fc18
vandana <vandana123annusah@gmail.com>
2021-11-29 15:01:57 +0530

    complete block BJaadf

--- block-BJaadf/code/index.js
+++ block-BJaadf/code/index.js
@@ -33,51 +33,77 @@ fruits.splice(1,1,'Pears')
 // Add the 'Kiwi' and 'Lemon' to the index 1 and 2 and shift the other element to next index (use splice to add element)
 fruits.splice(1,0,'Kiwi','Lemon')
 // Remove (slice) all the element from index 5
-fruits.slice(0,5);
+fruits.slice(5);
 // Create another array named moreFruits with values ['Berries', 'Melons']
 let moreFruits = ['Berries', 'Melons'];
 // Concat moreFruits into fruits array (re-assign so the value gets updated)
-
+fruits = fruits.concat(moreFruits)
 // Log the name of all fruit in console
-
+fruits.forEach(function (fruit) {
+  console.log(fruit);
+});
+fruits.forEach((fruit) => console.log(fruit));
 // Convert each fruit name to lowercase and log it
-
+fruits.forEach(function (fruit) {
+  console.log(fruit.toLowerCase());
+});
+fruits.forEach((fruit) => console.log(fruit.toLowerCase));
 // Convert all fruits name into lowercase and store in new array named lowercaseFruits
+let lowercaseFruits = [];
+fruits.forEach(function (fruit) {
+  lowercaseFruits.push(fruit.toLowerCase());
+});
+console.log(lowercaseFruits);
+
+ 
 
 // Convert all fruits name into uppercase and store in new array named uppercaseFruits
+let uppercaseFruits = [];
+fruits.forEach(function (fruit) {
+  uppercaseFruits.push(fruit.toUpperCase());
+});
+console.log(uppercaseFruits);
+
+
 
 let numbers = [1, 2, [3, 4]];
 let numbersTwo = [1, 2, [3, 4, [5, 6]]];
 let numbersThree = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
 
 // Convert the numbers array to one level element. Remove sub-arrays to individual elements and log them
-
+console.log(numbers.flat(1));
 // Convert the numbersTwo array to one level element. Remove all sub-arrays to individual elements and log them.
 // (for multiple level flat accepts argument by default it's 1 `array.flat(2)`)
-
+console.log(numbersTwo.flat(2));
 // Convert the numbersThree array to one level element.
+numbersThree.flat(Infinity);
 // Remove all sub-arrays to individual elements, log them and update the value of numbersThree to the new flat array.
+
+numbersThree = numbersThree.flat(Infinity);
 
 // Use forEach to log all the elements of numberThree array
 
+numbersThree.forEach((num) => console.log(num));
+
 // Create a new variable named doubleNumbers that store the numberThree array (each element should be multiplied by 2). Use map
 
+let doubleNumbers = numbersThree.map((number) => number * 2);
+
 // Create a new variable named tripleNumbers that store the numberThree array (each element should be multiplied by 3). Use map
-
+let tripleNumbers = numbersThree.map((number) => number * 2);
 // Create a new variable named halfNumbers that store the numberThree array (each element should be divided by 2). Use map
-
+let halfNumbers = numbersThree.map((number) => number / 2);
 // Create a new variable named oddNumbers that store all the odd numbers in numbersThree array
-
+let oddNumbers = numbersThree.filter((number) => number % 2 !== 0);
 // Create a new variable named evenNumbers that store all the even numbers in numbersThree array
-
+let evenNumbers = numbersThree.filter((number) => number % 2 === 0);
 // Find the index of 10 in numbersThree array
-
+numbersThree.find((number) => number === 0);
 // Reverse the values of numbersThree array
-
+numbersThree.reverse();
 // Reverse the values of numbersTwo array
-
+numbersTwo.reverse();
 // Join all fruits with '-', convert to uppercase and log it
-
+console.log(fruits.join("-").toUpperCase());
 // Join all fruits with '&', convert to lowercase and log it
-
-
+console.log(fruits.join("-&").toLowerCase ());
